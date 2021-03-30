244aa21cd8bb0f33a5b2e8d812f4e3ce564987a7
99cshea <51538025+99cshea@users.noreply.github.com>
2021-03-30 18:20:29 -0400

    this is fine everything is fine

--- public/scripts.js
+++ public/scripts.js
@@ -1,23 +1,24 @@
-/* eslint-disable no-console */
-import express from "express";
-import sequelize from "sequelize";
-import apiRoutes from "../routes/apiRoutes.js";
-import db from "../database/initializeDB.js";
+async function getData() {
+    const request = await fetch('/api/dining');
+    const restaurant = await request.json();
+    const arrayDining = restaurant.data();
+    const diningTable = document.querySelector('.table');
+    console.table(diningTable);
 
-const app = express();
 
-const router = express.Router();
+arrayDining.forEach((index) => {
+    const appendItem = document.createElement('tr');
+    appendItem.innerHTML = `
+        <td>${index.hall_id}</td>
+        <td>${index.hall_name}</td>
+        <td>${index.hall_address}</td>
+    `;
+    diningTable.append(appendItem);
+    });
+}
 
-const PORT = process.env.PORT || 3000;
+async function windowActions() {
+    const data = await getData();
+}
 
-router.get("/dining", async (req, res) => {
-    try {
-      const halls = await db.DiningHall.findAll();
-      const reply =
-        halls.length > 0 ? { data: halls } : { message: "no results found" };
-      res.json(reply);
-    } catch (err) {
-      console.error(err);
-      res.error("Server error");
-    }
-  });
+window.onload = windowActions;
